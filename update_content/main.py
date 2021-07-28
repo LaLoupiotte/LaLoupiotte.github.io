eeaa186c1e2bc2e1bef50e3fe417eb5aec70bf67
LaLoupiotte <74879238+LaLoupiotte@users.noreply.github.com>
2021-07-28 11:55:43 +0100

    little upgrades

--- update_content/main.py
+++ update_content/main.py
@@ -1,24 +1,24 @@
-from datetime import datetime
+import datetime
 
 def create_post(title, text, filename, subtitle=None):
     input_file = open("./base.html", "rt")
     output_file = open(filename, "wt")
     data = input_file.read()
-    data = data.replace('<!--Title-->', title)
+    data = data.replace('<!--Title-->', f'<h1>{title}</h1>')
     if subtitle != None:
-        data = data.replace('<!--Subtitle-->', subtitle)
-    data = data.replace('<!--Date-->', datetime.date())
+        data = data.replace('<!--Subtitle-->', f'<h2 class="subheading">{subtitle}</h2>')
+    data = data.replace('<!--Date-->', f'<span class="meta">Posted by <a href="./about.html">Guilherme Miranda Martins</a> on {datetime.date.today()}</span>')
     data = data.replace('<!--Texte-->', get_html_text(text))
     input_file.close()
     output_file.write(data)
     output_file.close()
 
 def get_html_text(text):
-    pass
+    return "hello text"
 
 
 #<h2 class="subheading">Problems look mighty small from 150 miles up</h2>
-#<span class="meta">Posted by <a href="#">Guilherme Miranda Martins</a> on August 24, 2014</span>
+#<span class="meta">Posted by <a href="./about.html">Guilherme Miranda Martins</a>""</span>.format(datetime.date())
+
 
 create_post("titre", "texte", "fichier.html")
-print(datetime.now())
