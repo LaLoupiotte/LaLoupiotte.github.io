089eb00785975e638ce485a176b57030e4bbdefc
LaLoupiotte <74879238+LaLoupiotte@users.noreply.github.com>
2021-07-29 16:57:21 +0100

    py index change 01

--- update_content/main.py
+++ update_content/main.py
@@ -1,7 +1,7 @@
 import datetime
 
 def create_post(title, subtitle, text, filename):
-    input_file = open("./base.html", "rt")
+    input_file = open("./post_base.html", "rt")
     output_file = open(filename, "wt")
     data = input_file.read()
     data = data.replace('<!--Title-->', f'<h1>{title}</h1>')
@@ -20,7 +20,7 @@ def get_html():
         subtitle = lines.pop(0)
         filename = lines.pop(0)
         filename = filename[filename.index(":")+2:] +f"{datetime.date.today()}.html"
-        filename = "../html_pages/" + filename
+        filename = "../html_pages/articles/" + filename
         text = ""
         print(lines)
         for line in lines:
@@ -33,9 +33,23 @@ def get_html():
     return title, subtitle, text, filename
         
 
-
-#blockquote
-
-
 title, subtitle, text, filename = get_html()
 create_post(title, subtitle, text, filename)
+
+content_html_inject = """
+                <div class="post-preview">
+                    <a href="post.html">
+                        <h2 class="post-title">
+                            The reasons why I didn't make my website on Squarespace
+                        </h2>
+                        <!--Here-->
+                    </a>
+                    <p class="post-meta">Posted by <a href="./about.html">Guilherme Miranda Martins</a> on September 24, 2014</p>
+                </div>
+                <hr>
+"""
+html_subtitle_inject = """
+                        <h3 class="post-subtitle">
+                            Is it still relevant to use programming in order to make a blog ? 
+                        </h3>
+"""
